4c9ca1362c8d829e1f17846e75e33323a002d6bd
Mikael Lönn <mikael.lonn@outlook.com>
2017-10-30 12:43:02 +0100

    Add sceletion file for first lecture

---
+++ lectures/Intro.hs
@@ -0,0 +1,85 @@
+-- | Some small examples to introduce Haskell.
+-- Functional Programming course 2017.
+-- Thomas Hallgren
+
+{-
+This is just a skeleton, the function definitions will be filled in
+during the first lecture.
+-}
+
+       
+---------------------------------------------------------------------------------- * Currency conversion
+
+exchangeRate = 9.7145  -- SEK / EUR
+
+--toEUR 
+
+--toSEK
+
+
+
+--prop_exchange
+
+
+-- automated random testing with QuickCheck
+
+
+
+--------------------------------------------------------------------------------
+-- * Definition by cases
+
+--absolute x
+
+
+-- local definitions
+
+--------------------------------------------------------------------------------
+-- * Definition by recursion
+-- The power function n^k
+
+--power n k
+
+--prop_power
+
+
+-- intersecting lines
+
+--intersect
+
+--------------------------------------------------------------------------------
+-- * Tuples
+
+--examplePair = 
+
+--exampleTriple = 
+
+--exampleFunction (b,n,s) = 
+
+--------------------------------------------------------------------------------
+-- * List
+
+snacks = "Spam"
+
+dinner = [snacks, "Fish", "Chips", snacks, snacks, "Pudding"]
+
+--summary :: [String] -> String
+--summary
+
+-- | Computing the length of a list
+--len
+
+--last'
+
+--------------------------------------------------------------------------------
+ -- * List comprehensions
+
+--ex1
+
+--doubles xs
+
+--ex2
+
+--ex3
+
+
+--pythag n =
