d348e5a074dd3c220b10735b1914578278a24ba7
Mikael Lönn <mikael.lonn@outlook.com>
2017-10-31 15:18:34 +0100

    Changes

--- lab1/Lab1-mlonn.hs
+++ lab1/Lab1-mlonn.hs
@@ -22,23 +22,18 @@ power2 n 0 = 1
 power2 n k | even k = power2 (n*n) (div k 2)
 power2 n k | odd k = n * power2 n (k-1)
 
-{-
-Part 4
-A.
-Test Cases:
-| n | 0 | 0 | 1 | 2 | 3 | 6 | 8 | 5  | 17 |
-| k | 0 | 1 | 0 | 3 | 2 | 8 | 6 | 17 | 5  |
--}
+
+--Part 4
+--A.
+k = [0, 2, 5, 5789, 8962] --test 0, odd, even, large odd, large even
+n = k ++ [-3, -7] -- add negative for n
 -- B.
 prop_powers :: Integer -> Integer -> Bool
 prop_powers n k = (power n k' == power1 n k') == (power n k' == power2 n k')
   where
     k' = abs k
 
-{-- C
-nk is a list of values for n and k
--}
-nk = [0, 1, 2, 100, 99, 1024]
-test = [prop_powers n k | n <- nk, k <- nk]
+-- C
+test = [prop_powers n k | n <- n, k <- k]
 -- D
 -- prop_powers worked the first time.
