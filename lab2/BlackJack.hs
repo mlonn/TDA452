7b186c9bdeac11b0eb846c9dd873d005eeddbe9f
Mikael Lönn <mikael.lonn@outlook.com>
2017-11-01 10:55:27 +0100

    value function

--- lab2/BlackJack.hs
+++ lab2/BlackJack.hs
@@ -2,9 +2,37 @@ module BlackJack where
 import Cards
 import RunGame
 
---empty :: Hand
+empty :: Hand
+empty = Empty
 
---value :: Hand -> Integer
+value :: Hand -> Integer
+value Empty           = 0
+value (Add card hand) | handValue <= 21 = handValue
+                      | handValue > 21 = handValue - aceValue
+    where
+        handValue = valueRank (rank card) + value hand
+        aceValue = numberOfAces (Add card hand) * 10
+
+valueRank :: Rank -> Integer
+valueRank Ace             = 11
+valueRank King            = 10
+valueRank Queen           = 10
+valueRank Jack            = 10
+valueRank (Numeric value) = value
+
+numberOfAces :: Hand -> Integer
+numberOfAces Empty                              = 0
+numberOfAces (Add card hand) | rank card == Ace = 1 + numberOfAces hand
+                             | otherwise        = numberOfAces hand
+
+
+hand = Add (Card Ace Hearts) 
+        (Add (Card Ace Hearts)
+        (Add (Card Ace Spades) Empty))
+
+hand2 = Add (Card Ace Hearts) 
+        (Add (Card Jack Hearts)
+        (Add (Card (Numeric 3) Spades) Empty))
 
 --gameOver :: Hand -> Bool
 
