0a3b32d14f66fa779ccc8a267b41722ab1fb8a38
Mikael Lönn <mikael.lonn@outlook.com>
2017-11-02 10:13:23 +0100

    Add sceletion for Week2 ecercises

---
+++ exercises/mlonn/Week2.hs
@@ -0,0 +1,30 @@
+-- | 1.(*) The Maximum Function
+-- maxi x y returns the maximum of x and y
+
+
+-- | 2. Sum of squares.
+-- sumsq n returns 1*1 + 2*2 + ... + n*n
+
+-- | 3. (*) The Towers of Hanoi
+--
+
+-- | 4. Fibonacci Numbers
+
+-- | 5. Factors.
+-- smallestFactor n
+
+-- | 6. (*) Multiplying list elements
+-- multiply :: Num a => [a] -> a
+
+-- | 7. Avoiding Duplicates
+-- duplicates :: Eq a => [a] -> Bool
+-- removeDuplicates :: Eq a => [a] -> [a]
+--prop_duplicatesRemoved :: [Integer] -> Bool
+--prop_duplicatesRemoved xs = not (duplicates (removeDuplicates xs))
+
+-- | 8. Testing
+
+-- | 9. (*) Defining Types
+-- daysInMonth :: Month -> Integer -> Integer
+-- validDate :: Date -> Bool
+-- tomorrow :: Date -> Date
