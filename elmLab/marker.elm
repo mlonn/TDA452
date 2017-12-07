17918d0bca719179e80c2bda885cd71df4e054e9
Mikael Lönn <mikael.lonn@outlook.com>
2017-12-07 12:12:22 +0100

    generate markers :hankey:

--- elmLab/marker.elm
+++ elmLab/marker.elm
@@ -15,11 +15,15 @@ type Symbol = Moon | Planet | Star | Gear
 {-| -}
 symbols : List Symbol
 symbols = [Moon, Planet, Star, Gear]
+
+markerCombinations : List ( Symbol, Color )
+markerCombinations = List.concat <| List.map2 (\s cs -> List.map (\c -> (s, c)) cs) symbols <| List.map (\x -> colors) colors
 {-| -}
 markersGenerator : Int -> Generator (List Marker)
-markersGenerator i = flattenList <| List.concat <| List.map (\s -> (List.map2 (\c -> markerGenerator i c) colors) s )symbols
+markersGenerator i = flattenList <| List.map (markerGenerator i) markerCombinations
 
-markerGenerator : Int -> Color -> Symbol -> Generator Marker
-markerGenerator i c s = map (mkMarker c s) (int 0 i)
+markerGenerator : Int -> (Symbol, Color) -> Generator Marker
+markerGenerator i (s, c) = map (mkMarker c s) (int 0 i)
+
 mkMarker : Color -> Symbol -> Int -> Marker
 mkMarker c s i =   {c = c, s = s, i = i}
